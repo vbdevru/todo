3222b57bafd1d61e9aa8af961d4541c2b885a406
vbdevru <vbdevru@mail.ru>
2022-12-13 23:37:42 +0300

    #2 Test commit

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	handlers := new(handler.Handler)
-
+    // test
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
