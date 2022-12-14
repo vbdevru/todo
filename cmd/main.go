6d1888dbf31e671e75b922be2a404ee67128e80c
vbdevru <vbdevru@mail.ru>
2022-12-14 23:42:46 +0300

    #7 Test

--- cmd/main.go
+++ cmd/main.go
@@ -16,6 +16,6 @@ func main() {
     // test
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
-		log.Fatalf("error occured while running https server: %s", err.Error())
+		log.Fatalf("errors occured while running https server: %s", err.Error())
 	}
 }
